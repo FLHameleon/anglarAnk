c325ba951696fba2514ecbf5662b18f07a95f678
FLH <savabg0826@gmail.com>
2021-06-30 00:16:44 +0300

    redaxdemoversion

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { interval, timer } from 'rxjs';
 import {Todo, TodosService} from '../shared/todos.service';
 
 // @NgModule({
@@ -18,11 +19,42 @@ export class TodoFormComponent implements OnInit {
   framework: string = '';
   frameworkVersion: string = '';
   email: string = '';
+  flagversion: boolean = false;
+
+  mast: Array<string> = ['', 'angular', 'react', 'vue'];
+  versins: Array<string> = [];
+
+  vaiant1: Array<string> = ['1.1.1', '1.2.1', '1.3.3'];
+  vaiant2: Array<string> = ['2.1.2', '3.2.4', '4.3.1'];
+  vaiant3: Array<string> = ['3.3.1', '5.2.1', '5.1.3'];
+
 
   constructor(private todosService: TodosService) {
   }
 
   ngOnInit() {
+  }
+
+  frameSelect() {
+    if(this.framework) this.flagversion = true
+    else this.flagversion = false
+
+    if(this.flagversion) {
+      switch(this.framework) {
+        case 'angular': {
+          this.versins = this.vaiant1
+          break;
+        }
+        case 'react': {
+          this.versins = this.vaiant2
+          break;
+        }
+        case 'vue': {
+          this.versins = this.vaiant3
+          break;
+        }
+      }
+    }
   }
 
   pushUserData() {
@@ -36,7 +68,15 @@ export class TodoFormComponent implements OnInit {
       email: this.email
     }
 
-    this.todosService.pushUserData(todotodo)
+
+    if(this.email == "test@test.test") {
+      const obs$ = timer(2000)
+      obs$.subscribe(() => {
+        alert('В базе данных почта test@test.test уже есть ')
+      })
+    }
+    else this.todosService.pushUserData(todotodo)
+
     this.firstName = ''
     this.lastName = ''
     this.dateOfBirth = ''
@@ -44,5 +84,6 @@ export class TodoFormComponent implements OnInit {
     this.frameworkVersion = ''
     this.email = ''
   }
+
 
 }
